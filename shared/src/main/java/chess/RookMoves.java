ca8c88552a435be4ccf81c341e4f792b26a0b49c
Karly James <kjames1a@byu.edu>
2025-05-05 11:20:27 -0600

    RookMoves class created

---
+++ shared/src/main/java/chess/RookMoves.java
@@ -0,0 +1,43 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMoves {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> chessMoves = new ArrayList<>();
+        rookMovesCapture(myPosition, chessMoves, 1, 0, board);
+        rookMovesCapture(myPosition, chessMoves, -1, 0, board);
+        rookMovesCapture(myPosition, chessMoves, 0, 1, board);
+        rookMovesCapture(myPosition, chessMoves, 0, -1, board);
+        return chessMoves;
+    }
+
+    private boolean OutOfBounds (ChessPosition myPosition) {
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        return (row < 1 || col < 1 || row > 8 || col > 8);
+    }
+
+    private void rookMovesCapture (ChessPosition myPosition, Collection<ChessMove> chessMoves, int rowMove, int colMove, ChessBoard board) {
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        int newRow = row + rowMove;
+        int newCol = col + colMove;
+        while (!OutOfBounds(new ChessPosition(newRow, newCol))) {
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessPiece mainTeam = board.getPiece(myPosition);
+            ChessPiece oppTeam = board.getPiece(newPos);
+            if (oppTeam == null) {
+                chessMoves.add(new ChessMove(myPosition, newPos, null));
+            } else {
+                if (oppTeam.getTeamColor() != mainTeam.getTeamColor()) {
+                    chessMoves.add(new ChessMove(myPosition, newPos, null));
+                }
+                break;
+            }
+            newRow += rowMove;
+            newCol += colMove;
+        }
+    }
+}
