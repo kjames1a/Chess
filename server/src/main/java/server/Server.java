05459fb41250000f650be19e08e2fd9552c077f8
Karly James <kjames1a@byu.edu>
2025-05-28 13:41:21 -0600

    Server code update

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,27 +1,40 @@
 package server;
 
-import chess.ChessGame;
 import com.google.gson.Gson;
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
-import dataaccess.UserDAO;
-import model.AuthData;
-import model.UserData;
-import model.GameData;
+import dataaccess.*;
+import exceptions.ResponseException;
+import dataaccess.DataAccessException;
 import spark.*;
 
 import java.util.Map;
 
 public class Server {
-    UserDAO userDAO = new UserDAO();
-    AuthDAO authDAO = new AuthDAO();
-    GameDAO gameDAO = new GameDAO();
-    RegisterHandler registerHandler = new RegisterHandler(userDAO, authDAO);
-    LoginHandler loginHandler = new LoginHandler(userDAO, authDAO);
-    LogoutHandler logoutHandler = new LogoutHandler(authDAO);
-    CreateGameHandler createGameHandler = new CreateGameHandler(gameDAO, authDAO);
-    ListGameHandler listGameHandler = new ListGameHandler(gameDAO, authDAO);
-    JoinGameHandler joinGameHandler = new JoinGameHandler(gameDAO, authDAO);
+    private UserSQL userSQL;
+    private AuthSQL authSQL;
+    private GameSQL gameSQL;
+    private RegisterHandler registerHandler;
+    private LoginHandler loginHandler;
+    private LogoutHandler logoutHandler;
+    private CreateGameHandler createGameHandler;
+    private ListGameHandler listGameHandler;
+    private JoinGameHandler joinGameHandler;
+
+
+    public Server() {
+        try {
+            this.userSQL = new UserSQL();
+            this.authSQL = new AuthSQL();
+            this.gameSQL = new GameSQL();
+            this.registerHandler = new RegisterHandler(userSQL, authSQL);
+            this.loginHandler = new LoginHandler(userSQL, authSQL);
+            this.logoutHandler = new LogoutHandler(authSQL);
+            this.createGameHandler = new CreateGameHandler(gameSQL, authSQL);
+            this.listGameHandler = new ListGameHandler(gameSQL, authSQL);
+            this.joinGameHandler = new JoinGameHandler(gameSQL, authSQL);
+        } catch (ResponseException | DataAccessException ex) {
+            throw new RuntimeException("Server failed", ex);
+        }
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -42,26 +55,29 @@ public class Server {
 
         Spark.put("/game", joinGameHandler::handle);
 
+        Spark.exception(ResponseException.class, this::exceptionHandler);
 
         Spark.awaitInitialization();
         return Spark.port();
     }
 
-
-private Object dbRoute(Request req, Response res) {
-    authDAO.deleteAllAuthTokens();
-    userDAO.deleteAllUsers();
-    gameDAO.deleteAllGames();
+private Object dbRoute(Request req, Response res) throws ResponseException {
+    try {
+        authSQL.deleteAllAuthTokens();
+        userSQL.deleteAllUsers();
+        gameSQL.deleteAllGames();
+    } catch (DataAccessException ex) {
+        throw new ResponseException(500, "Error: Problem contacting database");
+    }
     return postList(req, res);
 }
 
 private Object postList(Request req, Response res) {
     res.type("application/json");
-    return new Gson().toJson(Map.of("name", authDAO));
+    return new Gson().toJson(Map.of("name", "Cleared"));
 }
 
-
-    public static void main(String [] args) {
+    public static void main(String [] args) throws ResponseException, DataAccessException {
         Server server= new Server();
         server.run (8080);
         System.out.println("Connected to server");
@@ -70,5 +86,10 @@ private Object postList(Request req, Response res) {
     public static void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void exceptionHandler(ResponseException ex, Request req, Response res) {
+        res.status(ex.statusCode());
+        res.body(ex.toJson());
     }
 }
