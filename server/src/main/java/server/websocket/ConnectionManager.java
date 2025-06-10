51ce57a16b9e3f45e14dc4b09d8c46e529f400aa
Karly James <kjames1a@byu.edu>
2025-06-09 23:39:00 -0600

    Websocket connection

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,38 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String userName, Session session) {
+        var connection = new Connection(userName, session);
+        connections.put(userName, connection);
+    }
+
+    public void remove(String visitorName) {
+        connections.remove(visitorName);
+    }
+
+    public void broadcast(String excludeVisitorName, NotificationMessage notification) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.userName.equals(excludeVisitorName)) {
+                    c.send(notification.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+        for (var c : removeList) {
+            connections.remove(c.userName);
+        }
+        }
+}
